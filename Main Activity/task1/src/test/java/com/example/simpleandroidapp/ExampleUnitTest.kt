9ae35411275ba4e8d115d9db0919916f0ea11d1a
Igor <igor.gerasimov@jetbrains.com>
2021-08-05 10:11:14 +0300

    Added 3 prototype tasks

---
+++ Main Activity/task1/src/test/java/com/example/simpleandroidapp/ExampleUnitTest.kt
@@ -0,0 +1,12 @@
+package com.example.simpleandroidapp
+
+import org.junit.Assert
+import org.junit.Test
+
+class ExampleUnitTest {
+    @Test
+    fun testSolution() {
+        //TODO: implement your test here
+        Assert.assertTrue("Tests not implemented for the task", true)
+    }
+}
